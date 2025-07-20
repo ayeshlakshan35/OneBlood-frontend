13356dad1070af4fe78b636027c50ed1512f3209
sasi708 <sasindu013@gmail.com>
2025-07-20 13:26:35 +0530

    add footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
 import Login from '../Pages/Login'
 import Services from '../Component/Home/Services'
+import Footer from '../Component/Footer/Footer'
 
 export default function Home() {
   return (
     <div>
         home
         <Services />
+        <Footer/>
 
     
     </div>
