505ceda70ee8eace47bb6e73be41cb0e94ccb24b
ayeshlakshan35 <ict22033@std.uwu.ac.lk>
2025-08-09 13:10:01 +0530

    last update

--- src/Pages/FindBlood.jsx
+++ src/Pages/FindBlood.jsx
@@ -9,7 +9,7 @@ const districts = [
   "Kandy", "Matale", "Nuwara Eliya",
   "Galle", "Matara", "Hambantota",
   "Jaffna", "Kilinochchi", "Mannar", "Vavuniya", "Mullaitivu",
-  "Batticaloa", "Ampara", "Trincomalee",git 
+  "Batticaloa", "Ampara", "Trincomalee",
   "Kurunegala", "Puttalam",
   "Anuradhapura", "Polonnaruwa",
   "Badulla", "Monaragala",
